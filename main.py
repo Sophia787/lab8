4705d9a1a27fdf00b4d2b215fa1f7a4d2c21bf70
Sophia787 <91783576+Sophia787@users.noreply.github.com>
2021-11-17 16:18:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+def kbig(nums, k):
+    for i in nums:
+        count_more = 0          #кол-во элементов которые больше элемента i
+        for j in nums:
+            if i < j:
+                count_more += 1
+        if count_more == k-1:   #если кол-во элементов равно k-1, то мы нашли нужное число и выводим i 
+            return i
 
+
+while True:
+    print("Вход (введите значения списка через пробел): ")
+    nums = [int(i) for i in input('nums = ').split()]
+    k = int(input('Введите k = '))
+    print("Вывод: ", kbig(nums, k))
